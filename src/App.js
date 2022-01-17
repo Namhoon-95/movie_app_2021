6250fb6f2080d2424b689f1960f66c51b336ebf0
Namhoon-95 <jnh9575@naver.com>
2022-01-17 10:41:56 +0900

    최종완성...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Home from './routes/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 
 
 
 function App(){
-  return <Home/>;
+  return (
+    <BrowserRouter>
+      <Navigation/>
+      <Routes>
+        <Route path="/" exact={true} element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/movie-detail" element={Detail}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 
